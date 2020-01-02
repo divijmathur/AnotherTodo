38dbe90c944af35dd47467b4d47f6646a0cdafcb
Mona Lisa <divijmathur@gmail.com>
2020-01-01 22:36:11 -0800

    Updates

---
+++ precache-manifest.023dcfb301d8cee1846c23e649578edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06377b6532a7b718320d8370df094aa",
+    "url": "/divijmathur/AnotherTodo/index.html"
+  },
+  {
+    "revision": "659e9bf077cd79b0a259",
+    "url": "/divijmathur/AnotherTodo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30b9a82db9aab372e0aa",
+    "url": "/divijmathur/AnotherTodo/static/js/2.fa1c7be4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/divijmathur/AnotherTodo/static/js/2.fa1c7be4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "659e9bf077cd79b0a259",
+    "url": "/divijmathur/AnotherTodo/static/js/main.4f34671e.chunk.js"
+  },
+  {
+    "revision": "ce9592de7793c80de13c",
+    "url": "/divijmathur/AnotherTodo/static/js/runtime-main.e152a5b0.js"
+  }
+]);
